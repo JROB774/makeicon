a737b9456d57c409de5ef87111fb9b0545b7bcf8
Joshua Robertson <joshrobertson774@gmail.com>
2022-06-29 23:05:04 +0100

    Improve the API + improve Windows icon generation to support multiple inputs.

--- iconic.hpp
+++ iconic.hpp
@@ -2,6 +2,10 @@
 
 #include <stdint.h>
 
+#include "iconic_stb_image_read.h"
+#include "iconic_stb_image_resize.h"
+#include "iconic_stb_image_write.h"
+
 #ifndef ICONIC_API
 #define ICONIC_API
 #endif
@@ -17,5 +21,22 @@ typedef  int64_t s64;
 typedef    float f32;
 typedef   double f64;
 
-ICONIC_API void iconic_generate_win32_from_file(const char* output, const char* file_name);
-ICONIC_API void iconic_generate_win32_from_data(const char* output, const void* file_data, u64 file_size);
+typedef u32 Iconic_Format;
+enum Iconic_Format_
+{
+    ICONIC_FORMAT_NONE    =      0,
+    ICONIC_FORMAT_WIN32   = 1 << 0,
+    ICONIC_FORMAT_OSX     = 1 << 1,
+    ICONIC_FORMAT_ANDROID = 1 << 2,
+    ICONIC_FORMAT_IOS     = 1 << 3,
+};
+
+struct Iconic_Descriptor
+{
+    Iconic_Format icon_formats;
+    const char**  inputs;
+    u32           input_count;
+    const char*   output;
+};
+
+ICONIC_API bool iconic_generate_icon(const Iconic_Descriptor& desc);
