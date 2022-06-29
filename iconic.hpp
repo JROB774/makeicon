02a89b068043b0cf187c5e8b58ee4cd4b9e0849c
Joshua Robertson <joshrobertson774@gmail.com>
2022-06-29 21:54:45 +0100

    Add iconic library files.

---
+++ iconic.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef ICONIC_API
+#define ICONIC_API
+#endif
+
+ICONIC_API void iconic_generate_win32();
+ICONIC_API void iconic_generate_osx();
+ICONIC_API void iconic_generate_android();
+ICONIC_API void iconic_generate_ios();
