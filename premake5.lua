ae35bddf6f9828d8f861a070af400a692d219ead
unknown <jonathannrobson@outlook.com>
2022-08-01 17:27:11 +0100

    - added debugdir and include dir to premake

--- premake5.lua
+++ premake5.lua
@@ -25,6 +25,7 @@ workspace("makeicon" .. platform)
     cppdialect "C++17"
     location("build/" .. platform)
     targetdir("bin/" .. platform)
+    debugdir("bin/" .. platform)
     
     files
     {
@@ -34,5 +35,6 @@ workspace("makeicon" .. platform)
     
     includedirs
     {
+        "include",
         "third_party/stb"
     }
